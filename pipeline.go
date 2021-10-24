35ed1d96ac36c19ebb7cc0bb4493eec48c69da90
Jason Watson <jwatson@slashopt.net>
2021-10-24 18:00:50 +0200

    Vulcan, Go, and A Triangle, Part 8

--- pipeline.go
+++ pipeline.go
@@ -5,10 +5,15 @@ import (
 )
 
 type Pipeline struct {
-	Swapchain            vk.Swapchain
-	SwapchainImages      []vk.Image
-	SwapchainImageFormat vk.Format
-	SwapchainExtent      vk.Extent2D
+	Swapchain             vk.Swapchain
+	SwapchainImages       []vk.Image
+	SwapchainImageFormat  vk.Format
+	SwapchainExtent       vk.Extent2D
+	SwapchainImageViews   []vk.ImageView
+	SwapchainFramebuffers []vk.Framebuffer
+
+	RenderPass     vk.RenderPass
+	PipelineLayout vk.PipelineLayout
 }
 
 func NewPipeline(app *TriangleApplication, oldPipeline *Pipeline) *Pipeline {
@@ -125,15 +130,161 @@ func NewPipeline(app *TriangleApplication, oldPipeline *Pipeline) *Pipeline {
 
 	}()
 
+	// Create the image views.
+	imageViews := func() []vk.ImageView {
+		// Create the result object.
+		imageViews := make([]vk.ImageView, len(swapchainImages))
+
+		// Create one image view per image.
+		for k, img := range swapchainImages {
+			// Create the info object.
+			imageViewInfo := vk.ImageViewCreateInfo{
+				SType:    vk.StructureTypeImageViewCreateInfo,
+				Image:    img,
+				ViewType: vk.ImageViewType2d,
+				Format:   format.Format,
+				Components: vk.ComponentMapping{
+					R: vk.ComponentSwizzleIdentity,
+					G: vk.ComponentSwizzleIdentity,
+					B: vk.ComponentSwizzleIdentity,
+					A: vk.ComponentSwizzleIdentity,
+				},
+				SubresourceRange: vk.ImageSubresourceRange{
+					AspectMask:     vk.ImageAspectFlags(vk.ImageAspectColorBit),
+					BaseMipLevel:   0,
+					LevelCount:     1,
+					BaseArrayLayer: 0,
+					LayerCount:     1,
+				},
+			}
+
+			// Call the Vulkan function.
+			MustSucceed(vk.CreateImageView(app.device, &imageViewInfo, nil, &imageViews[k]))
+		}
+
+		// return the image views
+		return imageViews
+	}()
+
+	// Create the render pass.
+	renderPass := func() vk.RenderPass {
+		// Create the info object.
+		renderPassInfo := vk.RenderPassCreateInfo{
+			SType:           vk.StructureTypeRenderPassCreateInfo,
+			AttachmentCount: 1,
+			PAttachments: []vk.AttachmentDescription{
+				vk.AttachmentDescription{
+					Format:         format.Format,
+					Samples:        vk.SampleCount1Bit,
+					LoadOp:         vk.AttachmentLoadOpClear,
+					StoreOp:        vk.AttachmentStoreOpStore,
+					StencilLoadOp:  vk.AttachmentLoadOpDontCare,
+					StencilStoreOp: vk.AttachmentStoreOpDontCare,
+					InitialLayout:  vk.ImageLayoutUndefined,
+					FinalLayout:    vk.ImageLayoutPresentSrc,
+				},
+			},
+			SubpassCount: 1,
+			PSubpasses: []vk.SubpassDescription{
+				vk.SubpassDescription{
+					PipelineBindPoint:    vk.PipelineBindPointGraphics,
+					ColorAttachmentCount: 1,
+					PColorAttachments: []vk.AttachmentReference{
+						vk.AttachmentReference{
+							Attachment: 0,
+							Layout:     vk.ImageLayoutColorAttachmentOptimal,
+						},
+					},
+				},
+			},
+			DependencyCount: 1,
+			PDependencies: []vk.SubpassDependency{
+				vk.SubpassDependency{
+					SrcSubpass:    vk.SubpassExternal,
+					SrcStageMask:  vk.PipelineStageFlags(vk.PipelineStageColorAttachmentOutputBit),
+					DstStageMask:  vk.PipelineStageFlags(vk.PipelineStageColorAttachmentOutputBit),
+					DstAccessMask: vk.AccessFlags(vk.AccessColorAttachmentWriteBit),
+				},
+			},
+		}
+
+		// Create the result object.
+		var renderPass vk.RenderPass
+
+		// Call the Vulkan function.
+		MustSucceed(vk.CreateRenderPass(app.device, &renderPassInfo, nil, &renderPass))
+
+		// return the render pass
+		return renderPass
+	}()
+
+	// Create the framebuffers.
+	framebuffers := func() []vk.Framebuffer {
+		// Create the result object.
+		buffers := make([]vk.Framebuffer, len(imageViews))
+
+		// Create one framebuffer per image view.
+		for k, imgView := range imageViews {
+			// Create the info object.
+			bufferInfo := vk.FramebufferCreateInfo{
+				SType:           vk.StructureTypeFramebufferCreateInfo,
+				RenderPass:      renderPass,
+				AttachmentCount: 1,
+				PAttachments: []vk.ImageView{
+					imgView,
+				},
+				Width:  extent.Width,
+				Height: extent.Height,
+				Layers: 1,
+			}
+
+			// Call the Vulkan function.
+			MustSucceed(vk.CreateFramebuffer(app.device, &bufferInfo, nil, &buffers[k]))
+		}
+
+		// Return the framebuffers.
+		return buffers
+	}()
+
+	// Create the pipeline layout.
+	pipelineLayout := func() vk.PipelineLayout {
+		// Create the info object.
+		layoutInfo := vk.PipelineLayoutCreateInfo{
+			SType: vk.StructureTypePipelineLayoutCreateInfo,
+		}
+
+		// Create the result object.
+		var layout vk.PipelineLayout
+
+		// Call the Vulkan function.
+		MustSucceed(vk.CreatePipelineLayout(app.device, &layoutInfo, nil, &layout))
+
+		// Return the layout.
+		return layout
+	}()
+
 	// Create and return the pipeline
 	return &Pipeline{
-		Swapchain:            swapchain,
-		SwapchainImages:      swapchainImages,
-		SwapchainImageFormat: format.Format,
-		SwapchainExtent:      extent,
+		Swapchain:             swapchain,
+		SwapchainImages:       swapchainImages,
+		SwapchainImageFormat:  format.Format,
+		SwapchainExtent:       extent,
+		SwapchainImageViews:   imageViews,
+		SwapchainFramebuffers: framebuffers,
+		RenderPass:            renderPass,
+		PipelineLayout:        pipelineLayout,
 	}
 }
 
 func (pipeline *Pipeline) Cleanup(device vk.Device) {
+	for _, buffer := range pipeline.SwapchainFramebuffers {
+		vk.DestroyFramebuffer(device, buffer, nil)
+	}
+
+	vk.DestroyPipelineLayout(device, pipeline.PipelineLayout, nil)
+	vk.DestroyRenderPass(device, pipeline.RenderPass, nil)
+	for _, imgView := range pipeline.SwapchainImageViews {
+		vk.DestroyImageView(device, imgView, nil)
+	}
 	vk.DestroySwapchain(device, pipeline.Swapchain, nil)
 }
